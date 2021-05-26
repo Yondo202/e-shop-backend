c0839ba9932bb7de9e71fde96f2c7a3abea382d2
infosystems-mn <contact@infosystems.mn>
2021-05-26 10:53:47 +0800

    next

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '8c7d3b60c062853cbc91c856e278b67a'),
+    },
+  },
+});
