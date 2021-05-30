ed9a7a9f0d571824aeb0ba2e66bdd2716cb9ca31
huug buug <yondooo61@gmail.com>
2021-05-30 20:45:46 +0800

    first main push

--- config/server.js
+++ config/server.js
@@ -1,6 +1,6 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
+  port: env.int('PORT', 1338),
   admin: {
     auth: {
       secret: env('ADMIN_JWT_SECRET', '8c7d3b60c062853cbc91c856e278b67a'),
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '07fb2ea2-8ee5-4791-9e6d-121d5a43baa0'
+};
