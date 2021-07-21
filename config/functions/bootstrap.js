108efca98ca6293b9a9c0cb55ce89f5b025eec50
Yondo202 <yondooo61@gmail.com>
2021-07-21 18:36:26 +0800

    dd

--- config/functions/bootstrap.js
+++ config/functions/bootstrap.js
@@ -14,6 +14,8 @@ const pluralize = require('pluralize');
 module.exports = () => {
     // add any rules. First argument is Regex, second is string to replace
     pluralize.addPluralRule(/Бараанууд$/i, 'Бараа Бүтээгдэхүүн');
+    pluralize.addPluralRule(/Үндсэн_Ангилал$/i, 'Үндсэн Ангилал');
     pluralize.addPluralRule(/Дэд_Ангилал$/i, 'Дэд Ангилал');
     pluralize.addPluralRule(/Доод_Ангилал$/i, 'Доод Ангилал');
+    pluralize.addPluralRule(/Брэнд$/i, 'Брэндүүд');
 };
