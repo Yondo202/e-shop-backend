5a1ef5288b059a261981bf1b07589ebeb6c69265
Yondo202 <yondooo61@gmail.com>
2021-07-24 18:14:16 +0800

    dd

---
+++ config/hook.js
@@ -0,0 +1,12 @@
+module.exports = {
+    settings: {
+      // ...
+      algolia: {
+        enabled: true,
+        applicationId: 'KR0MBJN77V',
+        apiKey: 'c32ba27e22b7d643c5a52f91a111ab65',
+        debug: true,              // default: false
+        prefix: 'my_own_prefix'   // default: Strapi environment (strapi.config.environment)
+      },
+    }
+  };
--- config/server.js
+++ config/server.js
@@ -1,6 +1,7 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1338),
+  url:'https://bf201a2bb400.ngrok.io',
   admin: {
     auth: {
       secret: env('ADMIN_JWT_SECRET', '8c7d3b60c062853cbc91c856e278b67a'),
