8ded449395e8b86f0a2a3b30195bf99cc807233d
Yondo202 <yondooo61@gmail.com>
2021-07-28 16:12:53 +0800

    dd

--- config/server.js
+++ config/server.js
@@ -1,7 +1,7 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1338),
-  url:'https://28c89026ab48.ngrok.io',
+  url:'https://19e8ebcb75e7.ngrok.io',
   admin: {
     auth: {
       secret: env('ADMIN_JWT_SECRET', '8c7d3b60c062853cbc91c856e278b67a'),
